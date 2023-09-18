dd2cacfc346f114b440be4b8684c761291940563
joshuaagunsoye <64297090+joshuaagunsoye@users.noreply.github.com>
2023-09-18 05:47:19 -0300

    Update

--- my-app/src/Pages/HomePage.tsx
+++ my-app/src/Pages/HomePage.tsx
@@ -8,28 +8,49 @@ const HomePage = () => {
       <h1 className="header">Hi, I'm Agunsoye Joshua Oluwamuyiwa</h1>
       <div className="about">
         <p>
-          I'm a full-stack software engineer with 2 years of hands-on
-          professional experience in software development, process improvement,
-          on-time delivery of projects, with strong front-end
-          development experience. Self-motivated & committed individual who
-          loves challenges, quick to learn, and appreciates feedback.
+          Experienced Full-Stack Software Engineer | Proven Track Record in
+          Project Delivery | Front-End Specialist | Ready for Impactful Roles
         </p>
         <p>
-          I'm currently at CohabiLity, working on the frontend team that's
-          building the new webpage to enable a more efficient, objective webpage to make it easier and faster for users.
+          With 2 years of hands-on professional experience, I excel in
+          delivering high-quality software projects on time. My forte lies in
+          front-end development, where I've demonstrated exceptional skills in
+          creating intuitive and visually appealing user interfaces.
         </p>
         <p>
-          I previously worked on a project for Aqua-Advantage, where I worked as part of the front-end team 
-          to propose a new design and build from scratch a new website for the client.
+          Currently, I'm a valuable member of the Cllctve Inc. team, working as
+          a Full-Stack Developer. Our mission is clear: to elevate user
+          experiences by revamping our webpage. My role spans both front-end and
+          back-end development, allowing me to drive the entire web development
+          process and ensure a seamless, feature-rich platform.
         </p>
-        <p>If you ever want to chat about or play basketball, or discuss an interesting project, 
-          please feel free to hit me up.</p>
+        <p>
+          I've also successfully contributed to client satisfaction as part of
+          the front-end team at Aqua-Advantage, where I led the proposal and
+          development of a brand-new website.
+        </p>
+        <p>
+          My commitment to excellence and my passion for staying ahead in the
+          tech world make me an ideal candidate for your team. Let's connect and
+          discuss how I can bring my skills to drive innovation and success in
+          your organization.
+        </p>
       </div>
 
       <div className="link-icons">
-        <a href="https://github.com/joshuaagunsoye" target="_blank"><AiFillGithub /></a>
-        <a href="https://www.linkedin.com/in/joshua-agunsoye-87a652144/" target="_blank"> <AiFillLinkedin /></a>
-        <a href="https://www.instagram.com/_currryy/" target="_blank"><AiFillInstagram /></a>
+        <a href="https://github.com/joshuaagunsoye" target="_blank">
+          <AiFillGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/joshua-agunsoye-87a652144/"
+          target="_blank"
+        >
+          {" "}
+          <AiFillLinkedin />
+        </a>
+        <a href="https://www.instagram.com/_currryy/" target="_blank">
+          <AiFillInstagram />
+        </a>
       </div>
     </div>
   );
--- my-app/src/Pages/WorkPage.tsx
+++ my-app/src/Pages/WorkPage.tsx
@@ -1,24 +1,125 @@
 import React from "react";
-import "../Pages/WorkPage.scss"
+import "../Pages/WorkPage.scss";
 const WorkPage = () => {
   return (
     <div className="workpg-contents">
       <div>
-        <p className="workplace">CohabiLity</p>
-        <p className="role">Front-End Developer (Contract through Mitacs), June 2022 - October 2022</p>
-        <p className="text">I work as a Frontend engineer on an internship basis to build quickly and cost-effectively.</p>
-        <p className="text">Technologies used: Javascript, React, SCSS, Redux.</p>
+        <p className="workplace">Cllctve Inc</p>
+        <p className="role">Full Stack Developer May 2023 - Present</p>
+        <p className="text">
+          I work as a Frontend engineer on an internship basis to build quickly
+          and cost-effectively.
+        </p>
+        <ul>
+          <li>Designed, Developed and Implemented new features</li>
+          <li>
+            Collaborated with Senior Developers to troubleshoot and debug code
+            issues
+          </li>
+          <li>
+            Developed Automated API test scripts with industry tools, including
+            tools such as Playwright.
+          </li>
+          <li>Created new backend services using docker.</li>
+          <li>Improving and Maintaining existing test framework.</li>
+        </ul>
+        <p className="text">
+          Technologies used: Javascript, React, SCSS, Redux.
+        </p>
       </div>
+
+      <div>
+        <p className="workplace">Live Art Dance </p>
+        <p className="role">
+          IT Administrator / Web Developer July 2023 - present{" "}
+        </p>
+
+        <ul>
+          <li>
+            Developed and managed websites to promote the organization's
+            activities and events.
+          </li>
+          <li>
+            Provided technical support to staff members, resolving IT-related
+            issues promptly and efficiently.
+          </li>
+          <li>
+            Managed and maintained the organization's IT infrastructure,
+            including servers, networks, and hardware.
+          </li>
+          <li>
+            Provided technical support to staff members, resolving IT-related
+            issues promptly and efficiently.
+          </li>
+          <li>
+            Implemented security measures to protect sensitive data and ensure
+            compliance with data protection regulations.
+          </li>
+          <li>
+            Assisted in the planning and execution of IT projects to enhance the
+            organization's technological capabilities.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <p className="workplace">CohabiLity</p>
+        <p className="role">
+          Front-End Developer (Contract through Mitacs), June 2022 - October
+          2022
+        </p>
+        <ul>
+          <li className="text">
+            Played a pivotal role in building web solutions efficiently during
+            an internship through Mitacs
+          </li>
+          <li className="text">
+            Developed and maintained web applications using Javascript, React,
+            SCSS, and Redux.
+          </li>
+        </ul>
+        <p className="text">
+          Technologies used: Javascript, React, SCSS, Redux.
+        </p>
+      </div>
+
       <div>
         <p className="workplace">Aqua-Advantage</p>
-        <p className="role">Front-End Developer (Full time),  May 2022 - June 2022</p>
-        <p className="text">I work as a Frontend engineer on a team to build rebuild customer webpage</p>
+        <p className="role">
+          Front-End Developer (Full time), May 2022 - June 2022
+        </p>
+        <ul>
+          <li className="text">
+            Contributed to the reconstruction of customer webpages to enhance
+            user experiences
+          </li>
+          <li className="text">
+            Utilized Javascript, React, and CSS to create visually appealing and
+            functional web interfaces.
+          </li>
+        </ul>
+
         <p className="text">Technologies used: Javascript, React, CSS.</p>
       </div>
+
       <div>
         <p className="workplace">Freelance</p>
         <p className="role"> Full Stack Developer, November 2021 - Till date</p>
-        <p className="text">Technologies used: Javascript, React, SCSS, Redux, MongoDB, Typescript, MySQL, Git, Jest, Firebase, Express.</p>
+        <ul>
+          <li className="text">
+            Successfully completed various freelance projects, demonstrating
+            expertise in Javascript, React, SCSS, Redux, MongoDB, Typescript,
+            MySQL, Git, Jest, Firebase, and Express
+          </li>
+          <li className="text">
+            Delivered custom solutions while adhering to project deadlines and
+            client requirements.
+          </li>
+        </ul>
+        <p className="text">
+          Technologies used: Javascript, React, SCSS, Redux, MongoDB,
+          Typescript, MySQL, Git, Jest, Firebase, Express.
+        </p>
       </div>
     </div>
   );
